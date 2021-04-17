5e0324260ced0a0e87b9447b14cdc989c6c8fdf0
Your NameAlex <alehr45@gmail.com>
2021-04-17 11:00:24 -0500

    initial files

---
+++ server.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
