619b0fa9861c69e392543bd89b954652722b09bb
Your NameAlex <alehr45@gmail.com>
2021-04-17 13:04:15 -0500

    added moment for date and time formatting

--- models/Thought.js
+++ models/Thought.js
@@ -1,22 +1,32 @@
 const { Schema, model } = require("mongoose");
+const moment = require('moment');
 
 const ThoughtSchema = new Schema({
   thoughtText: {
     type: String,
     required: true,
+    minLength: 1,
+    maxLength: 280
   },
   createdAt: {
     type: Date,
     default: Date.now,
+    get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
   },
-  username: {
+    username: {
     type: String,
     required: true,
   },
-  reactions: {
-    type: String,
+  
+  reactions:[]
+},
+{
+  toJSON: {
+    getters: true
   },
-});
+  id: false
+}
+);
 
 const Thought = model("Thought", ThoughtSchema);
 
--- models/User.js
+++ models/User.js
@@ -1,27 +1,22 @@
-const { Schema, model } = require('mongoose');
-
+const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema({
-    username: {
-      type: String,
-      required: true,
-      trim: true,
-      unique: true
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/.+@.+\..+/]
-    },
-    thoughts: {
-      
-    },
-    friends: {
-      type: String,
-    }
-  });
-  
-  const User = model('User', UserSchema);
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/],
+  },
+  thoughts: [],
+  friends: []
+});
 
-  module.exports = User;
+const User = model("User", UserSchema);
+
+module.exports = User;
