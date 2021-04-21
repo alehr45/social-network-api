e3a8eb08fe69d9f4504c0fceeed50d4bdcfc87bb
Your NameAlex <alehr45@gmail.com>
2021-04-21 10:40:53 -0500

    get all users populates thoughttext

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,8 +1,13 @@
-const { User } = require('../models');
+const { User, Thought } = require('../models');
 
 const userController = {
     getAllUser(req, res) {
         User.find({})
+        .populate([
+          { path: 'thoughts', select: "-__v" }
+          
+      ])
+      .select('-__v')
           .then(dbUserData => res.json(dbUserData))
           .catch(err => {
             console.log(err);
